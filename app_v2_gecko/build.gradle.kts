a67b3bee2ab1af72c8d6b95bcf22ffe22670c026
unouidol <federico.silenzi@alice.it>
2026-01-16 14:36:12 +0100

    v2 version

--- app_v2_gecko/build.gradle.kts
+++ app_v2_gecko/build.gradle.kts
@@ -25,33 +25,48 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "HISTORY_SECRET_KEY", "\"$historyKey\"")
+    }
 
+
+    buildFeatures {
+        compose = false
+        buildConfig = true
+    }
+
+
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("arm64-v8a", "armeabi-v7a")
+            isUniversalApk = false
+        }
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
+            isShrinkResources = false
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
+        // (opzionale) debug { ... }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
-    buildFeatures {
-        compose = false
-    }
 
 }
-
-dependencies {
-    implementation(libs.androidx.core.ktx)
+dependencies { implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
@@ -69,13 +84,9 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-
     implementation("androidx.fragment:fragment-ktx:1.8.5")
     implementation("androidx.viewpager2:viewpager2:1.1.0")
-
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-    implementation("com.github.bumptech.glide:glide:4.16.0")
-    // Source: https://mvnrepository.com/artifact/org.mozilla.geckoview/geckoview
+    implementation("com.github.bumptech.glide:glide:4.16.0") // Source: https://mvnrepository.com/artifact/org.mozilla.geckoview/geckoview
     implementation("org.mozilla.geckoview:geckoview:145.0.20251124145406")
-
 }
